71f493adfb531fa00d21abb4ec1a8b9dc1a44363
Jiahao, Woo <woojiahao1234@gmail.com>
2025-03-25 13:52:43 +0800

    Add post-download for problem set

--- post-download.sh
+++ post-download.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Generate a bunch of files and folders
+
+mkdir -p sensitive/
+mkdir {src,res}
+
+echo 'print("hello world!")' >>src/script.py
+echo 'KEY=secretshhhh' >>src/.env
+echo 'KEY=secretshhhh' >>src/.env
+curl 'https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1000&h=667&crop=1&resize=1000%2C667' --output res/hidden.png
+echo 'John\nAlice\nBob\nMichael' >>sensitive/names.txt
+touch sensitive/sensitive_{1..100}.txt
+
+echo 'sensitive/' >>.gitignore
+echo 'res/hidden.png' >>.gitignore
+echo 'src/.env' >>.gitignore
+echo '!sensitive/names.txt' >>.gitignore
+
+git add .gitignore
+git commit -m "Change"
+
 first_commit_hash=$(git log --reverse --pretty=format:'%h' | head -n 1)
 if [ $(git tag -l "git-mastery-start-$first_commit_hash") ]; then
   git tag -d "git-mastery-start-$first_commit_hash"
